0cd365398806e73741dc278dd98685c1d5f38b9f
adityamhn <aditya530026@gmail.com>
2020-11-01 12:27:16 +0530

    Updates

---
+++ precache-manifest.462d321a847ba253e8b86063166b1b3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a986c4d4d0441cee23ccec51bf08824",
+    "url": "/facerecognitionapp/index.html"
+  },
+  {
+    "revision": "ce3db6ac942b2ab09f4c",
+    "url": "/facerecognitionapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4c4206025b7db41bc6ec",
+    "url": "/facerecognitionapp/static/css/main.fbc6b8ba.chunk.css"
+  },
+  {
+    "revision": "ce3db6ac942b2ab09f4c",
+    "url": "/facerecognitionapp/static/js/2.d0414757.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionapp/static/js/2.d0414757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4206025b7db41bc6ec",
+    "url": "/facerecognitionapp/static/js/main.9b2e9434.chunk.js"
+  },
+  {
+    "revision": "5d2665b1dfdcad4e3236",
+    "url": "/facerecognitionapp/static/js/runtime-main.097120f7.js"
+  }
+]);
